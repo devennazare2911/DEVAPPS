3406cd69c73bcd33ac549a795e94a07e43ecbfd7
Ottoedge Mumbai <ottoedgemumbai@Karans-MacBook-Air.local>
2020-05-01 02:51:08 +0530

    CHange by Deven

--- App.js
+++ App.js
@@ -14,8 +14,10 @@ import {
   View,
   Text,
   StatusBar,
-  Button,
 } from 'react-native';
+
+
+import { Icon } from 'react-native';
 
 import {
   Header,
@@ -28,9 +30,7 @@ import {
 const App: () => React$Node = () => {
   return (
     <>
-
       <StatusBar barStyle="dark-content" />
-
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
@@ -41,23 +41,34 @@ const App: () => React$Node = () => {
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
           )}
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits. 
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
         </ScrollView>
       </SafeAreaView>
     </>
